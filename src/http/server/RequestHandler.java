40771710518b106cc878a08666d319a1f0b770cf
INSArthur <49277717+INSArthur@users.noreply.github.com>
2020-11-23 18:29:23 +0100

    serverName

--- src/http/server/RequestHandler.java
+++ src/http/server/RequestHandler.java
@@ -5,11 +5,13 @@ import java.util.List;
 public class RequestHandler {
     private Request request;
     private Response response;
+    private String serverName;
 
     /*Constructeur, instancie l’objet request avec l’objet en paramètre de RequestHandler*/
-    public RequestHandler(List<String> request){
+    public RequestHandler(List<String> request, String serverName){
         this.request = new Request(request);
         response = new Response();
+        this.serverName = serverName;
     }
 
     /*f(request) = response. Renvoie la réponse sous forme de bytes pour être directement envoyé*/
