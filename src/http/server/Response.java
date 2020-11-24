50bdd6c87876b0434f6c8d6a84cf546607b60e3b
Phanie Buonomo <fanny.buonomo@insa-lyon.fr>
2020-11-24 17:07:36 +0100

    meilleure façon de generer l'entete de la réponse

--- src/http/server/Response.java
+++ src/http/server/Response.java
@@ -9,6 +9,8 @@ public class Response {
   private byte[] body;
   private String response;
   private final String PROTOCOL = "HTTP/1.0 ";
+  private Map<String,String> parameters;
+  private List<String> allowParameters;
   private static Map<String,List<String>> typeToExtension;
   private static Map<Integer,String> codeToError;
 
@@ -19,33 +21,37 @@ public class Response {
   private static final String[] application = {"application","json", "pdf"};
   private static final String[][]     types = {audio,image,video,text,application};
 
-  private static final int[] codeError = {200,
-                                          201,
-                                          204,
-                                          400,
-                                          401,
-                                          403,
-                                          404,
-                                          415,
-                                          500,
-                                          501,
-                                          503};
-  private static final String[] messageError = {"OK",
-                                                "Created",
-                                                "No Content",
-                                                "Bad Request",
-                                                "Unauthorized",
-                                                "Forbidden",
-                                                "Not Found",
-                                                "Unsupported Media Type",
-                                                "Internal Server Error",
-                                                "Not Implemented",
-                                                "Service Unavailable"};
+  private static final int[] codeError = {
+    200,
+    201,
+    204,
+    400,
+    401,
+    403,
+    404,
+    415,
+    500,
+    501,
+    503};
+  private static final String[] messageError = {
+    "OK",
+    "Created",
+    "No Content",
+    "Bad Request",
+    "Unauthorized",
+    "Forbidden",
+    "Not Found",
+    "Unsupported Media Type",
+    "Internal Server Error",
+    "Not Implemented",
+    "Service Unavailable"};
 
   public Response(){
     this.response = PROTOCOL;
+    this.parameters = new HashMap<String,String>();
+    this.allowParameters = null;
   }
-  
+
   public void setResponseCode(int code){
     this.response += code+" "+getStringError(code);
     this.response += "\n";
@@ -53,40 +59,29 @@ public class Response {
 
   public void setExtension(String extension) {
     String type = this.findContentType(extension);
-    this.response +="Content-Type: "+type+"/"+extension;
-    this.response +="\n";
+    this.parameters.put("Content-type",type+"/extension");
   }
 
   public void addServerName(String server){
-    this.response +="Server: "+server;
-    this.response +="\n";
+    this.parameters.put("Server",server);
   }
 
-  public void addHTTPAllowedMethods(List<String> Methods){
+  public void addHTTPAllowedMethods(List<String> methods){
+    this.allowParameters = methods;
     this.response +="Allow: ";
-    int i=1;
-    int nbMethods = Methods.size();
-    for(String m: Methods){
-      this.response+=m;
-      if(i<nbMethods){
-        this.response+=", ";
-      }
-      i++;
-    }
-    this.response +="\n";
   }
 
   public void addRessource(String ressourceName) throws IOException{
     String filePath = "./ressources"+ressourceName;
     this.body  = Files.readAllBytes(Paths.get(filePath));
   }
-  
+
   public void writeRessource(String ressourceName, String content) throws IOException{
     BufferedWriter outFile = new BufferedWriter(new FileWriter("./ressources"+ressourceName));
     outFile.write(content);
     outFile.close();
   }
-  
+
   public void writeRessource(String ressourceName, Map<String,String> content) throws IOException{
     BufferedWriter outFile = new BufferedWriter(new FileWriter("./ressources"+ressourceName,true));
     for(Map.Entry<String,String> entry : content.entrySet()){
@@ -104,7 +99,6 @@ public class Response {
 
 
   public byte[] toBytes(){
-    this.finishHeader();
     byte[] header = this.getHeader();
     byte[] responseBytes = new byte[header.length+this.body.length];
     System.arraycopy(header,0,responseBytes,0,header.length);
@@ -112,22 +106,41 @@ public class Response {
     return responseBytes;
   }
 
-  private void finishHeader(){
-    this.response += "\n";
-  }
-
   private String getStringError(int code){
     return Response.codeToError.get(code);
   }
 
   private byte[] getHeader(){
-    return this.response.getBytes();
+    String header = this.completeHeader()
+      +"\n";
+    return header.getBytes();
+  }
+
+  private String completeHeader(){
+    String header = this.response
+                  + "\n";
+    if(this.allowParameters != null){
+      int i=1;
+      int nbMethods = allowParameters.size();
+      for(String m: allowParameters){
+        header +=m;
+        if(i<nbMethods){
+          header +=", ";
+        }
+        i++;
+      }
+      header +="\n";
+    }
+    for(Map.Entry<String,String> entry : this.parameters.entrySet()){
+      header += entry.getKey()+": "+entry.getValue()+"\n";
+    }
+    return header;
   }
 
   private String findContentType(String extension){
     for(List<String> extensionsType : Response.typeToExtension.values()){
       if(extensionsType.contains(extension)){
-          return extensionsType.get(0);
+        return extensionsType.get(0);
       }
     }
     return null;
